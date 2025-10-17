f44008c7a61952712128622e67d00302d4e6c4eb
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2025-10-17 10:18:36 +0200

    Inspect for ISegmentable2D updated

--- DiGi.Rhino.Geometry/Planar/Inspect/ISegmentable2D.cs
+++ DiGi.Rhino.Geometry/Planar/Inspect/ISegmentable2D.cs
@@ -1,20 +1,21 @@
 ﻿using DiGi.Rhino.Core.Classes;
 using DiGi.Rhino.Geometry.Planar.Classes;
+using Grasshopper.Kernel.Types;
 using System.Collections.Generic;
 
 namespace DiGi.Rhino.Geometry.Planar
 {
     public static partial class Inspect
     {
-        [Inspect("Rectangle2D", "Rectangle2D", "Rectangle2D")]
-        public static GooRectangle2D? Rectangle2D(this DiGi.Geometry.Planar.Interfaces.ISegmentable2D? segmentable2D)
+        [Inspect("Length", "Length", "Length")]
+        public static GH_Number? Length(this DiGi.Geometry.Planar.Interfaces.ISegmentable2D? segmentable2D)
         {
-            if(segmentable2D == null)
+            if (segmentable2D == null)
             {
                 return null;
             }
 
-            return new GooRectangle2D(DiGi.Geometry.Planar.Create.Rectangle2D(segmentable2D));
+            return new GH_Number(segmentable2D.Length);
         }
 
         [Inspect("Points", "Points", "Points")]
@@ -28,6 +29,17 @@ namespace DiGi.Rhino.Geometry.Planar
             return segmentable2D?.GetPoints()?.ConvertAll(x => new GooPoint2D(x));
         }
 
+        [Inspect("Rectangle2D", "Rectangle2D", "Rectangle2D")]
+        public static GooRectangle2D? Rectangle2D(this DiGi.Geometry.Planar.Interfaces.ISegmentable2D? segmentable2D)
+        {
+            if(segmentable2D == null)
+            {
+                return null;
+            }
+
+            return new GooRectangle2D(DiGi.Geometry.Planar.Create.Rectangle2D(segmentable2D));
+        }
+        
         [Inspect("Segments", "Segments", "Segments")]
         public static IEnumerable<GooSegment2D>? Segments(this DiGi.Geometry.Planar.Interfaces.ISegmentable2D? segmentable2D)
         {
